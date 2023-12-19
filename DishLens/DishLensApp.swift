9544374a3642c60e849ff1c7978ed569cec494ef
Kotaro Fukuo <haptaro.dev@gmail.com>
2023-12-18 17:18:03 -0800

    Initial Commit

---
+++ DishLens/DishLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  DishLensApp.swift
+//  DishLens
+//
+//  Created by Kotaro Fukuo on 2023/12/18.
+//
+
+import SwiftUI
+
+@main
+struct DishLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
